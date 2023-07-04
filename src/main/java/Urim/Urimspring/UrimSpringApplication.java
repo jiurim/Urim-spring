d531b8072f40f7073b29448b8e8357436e11f8ef
urim <jiurim0316@naver.com>
2023-07-04 22:51:16 +0900

    Initial commit

---
+++ src/main/java/Urim/Urimspring/UrimSpringApplication.java
@@ -0,0 +1,13 @@
+package Urim.Urimspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrimSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UrimSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Urim/Urimspring/UrimSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package Urim.Urimspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrimSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
